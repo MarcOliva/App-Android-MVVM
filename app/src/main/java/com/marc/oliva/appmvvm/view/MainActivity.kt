0f7f80ccf3940f4b5148fe0fbd3e3b0468bae399
Marc Oliva <ing.oliva@hotmail.com>
2019-09-12 10:27:59 -0500

    Add data binding expresions , view model and observable

--- app/src/main/java/com/marc/oliva/appmvvm/view/MainActivity.kt
+++ app/src/main/java/com/marc/oliva/appmvvm/view/MainActivity.kt
@@ -2,12 +2,43 @@ package com.marc.oliva.appmvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.marc.oliva.appmvvm.R
+import com.marc.oliva.appmvvm.model.Country
+import com.marc.oliva.appmvvm.viewmodel.CountryViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private var countryViewModel:CountryViewModel?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        //VIEW
+        setupBindings(savedInstanceState)
+    }
+
+    fun setupBindings(savedInstanceState: Bundle?){
+        val activityMainBinding:com.marc.oliva.appmvvm.databinding.ActivityMainBinding
+        =DataBindingUtil.setContentView(this,R.layout.activity_main)
+        countryViewModel= ViewModelProviders.of(this).get(CountryViewModel::class.java)
+
+        activityMainBinding.model=countryViewModel
+        setupListUpdate()
+    }
+    fun setupListUpdate(){
+        //Call countries
+        countryViewModel?.callCountries()
+        //getCountries
+        countryViewModel?.getCountries()?.observe(this, Observer {
+            countries:List<Country>->
+            CountryViewModel.mainActivity=this
+            countryViewModel?.setCountriesInRecyclerAdapter(countries)
+
+        })
     }
 }
